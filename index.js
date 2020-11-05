6359ef176c6d33c6572944fb0d883ec5de1c5a1c
Jennifer Sanders <jennys1000@gmail.com>
2020-11-05 18:50:09 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,27 @@
+function add (a, b) {
+number = a + b
+return number
+}
+function subtract (a, b) {
+  number = a - b
+  return number
+}
+function multiply (a, b) {
+number = a * b
+return number
+}
+
+function divide (a, b) {
+number = a / b
+return number
+}
+
+function increment(a) {
+   a += 1
+  return a
+}
+
+function decrement(a) {
+  a -= 1
+  return a
+}
